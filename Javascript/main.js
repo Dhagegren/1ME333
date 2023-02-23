bd5c3434bce1eb5d9b9714db1c066c41b1dd3fb8
Dhagegren <dh222ph@student.lnu.se>
2023-02-23 11:17:53 +0100

    qw

--- Javascript/main.js
+++ Javascript/main.js
@@ -22,7 +22,7 @@ window.addEventListener("devicemotion", function (event) {
         document.body.appendChild(log2);
 
 
-        if (gamma > 4 || gamma < -4) {
+        if (gamma > 20 || gamma < -20) {
           shaking= true;
         }
 
