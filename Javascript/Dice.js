eb53dfc2f4006187b76ff7aec892b352bcd98281
Dhagegren <dh222ph@student.lnu.se>
2023-02-28 00:08:07 +0100

    k

--- Javascript/Dice.js
+++ Javascript/Dice.js
@@ -1,7 +1,11 @@
-var Dice = function() {
+var Dice = function(sides) {
 
-    
-
+    this.sides = sides ||6;
 
 }
+
+Dice.prototype.roll = function(){
+    return Math.floor(Math.random()* this.sides)+1;
+};
+
 
