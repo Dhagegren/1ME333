a37e0725b78bd394729e45aefc9715112e1d1cdf
Dhagegren <dh222ph@student.lnu.se>
2023-03-05 23:58:43 +0100

    qd

--- Javascript/StartPage.js
+++ Javascript/StartPage.js
@@ -7,6 +7,7 @@ var StartPage = function(buttonText) {
 
     var button = new CreateButton(buttonText)
     button.addEventListener("click", Permission.givePermission);
+    button.style.display = 'none';
   }
 
   //var start = new Startpage("Start");
