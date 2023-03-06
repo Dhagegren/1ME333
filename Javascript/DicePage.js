3be0bfa4eeab435a118084bd7722dd450d1f9306
Daniel Hagegren <dh222ph@student.lnu.se>
2023-03-06 11:56:39 +0100

    asd

--- Javascript/DicePage.js
+++ Javascript/DicePage.js
@@ -5,16 +5,17 @@ var DicePage = function(value){
 
 
 for( var i=0; i<value; i++){
-    if(DiceArr.length > 0){
-        for(i=0; i<DiceArr.length; i++){
-            DiceArr[i].pop.remove;
+
+         for(i=0; i<DiceArr.length; i++){
+        DiceArr[i].pop.remove;
+    
         }
-    }
-    else{
-    var dice = new Dice();
-    DiceArr.push(dice);
-    dice.roll();
-    }
+        
+        var dice = new Dice();
+        DiceArr.push(dice);
+        dice.roll();
+    
+  
 }
 
 
