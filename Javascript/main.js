8832ebc1066df9ffd6e430f31ca14ac2f617705f
Dhagegren <dh222ph@student.lnu.se>
2023-03-06 13:13:50 +0100

    qdw

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,10 +1,12 @@
 //gör som main i webb5 statisk klass som kör igång av sig själv och startar startpage
+var Main = {
 
-
- function init() {
+ init: function() {
    var startpage = new StartPage("tryck på mig");
 
   }
+
+}
 window.addEventListener("load", init);
 
     
