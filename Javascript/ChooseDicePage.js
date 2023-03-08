86fc3d87d916237d6771afe72fbeb938b0c4edd3
Daniel Hagegren <dh222ph@student.lnu.se>
2023-03-08 13:07:33 +0100

    ad

--- Javascript/ChooseDicePage.js
+++ Javascript/ChooseDicePage.js
@@ -4,39 +4,44 @@
 var ChooseDicePage = function() {
  
 
-  delete Main.startpage; 
+      delete Main.startpage; 
 
- this.header = new CreateHead("Hur många tärningar vill du kasta?");
+     this.header = new CreateHead("Hur många tärningar vill du kasta?");
+  
+      // Create buttons for each number of dice
+      for (var i = 1; i <= 6; i++) {
+      this.button = new CreateButton(i);
+      this.button.setAttribute("class", "chooseBtn");
+   
+      // Eventlystnare på knappen
+      this.button.addEventListener("click", function(event) {
 
-  // Create buttons for each number of dice
-  for (var i = 1; i <= 6; i++) {
-  this.button = new CreateButton(i);
-  this.button.setAttribute("class", "chooseBtn");
 
-  // Eventlystnare på knappen
-  this.button.addEventListener("click", function(event) {
+        this.header = document.getElementsByTagName("h2");
+        for(i=0; i<this.header.length; i++){
+        this.header[i].style.display = "none"
+        }
 
+        this.numDice = parseInt(event.target.textContent);
+        //skapar en dicePage där tärningarna kommer att vara
+    
+      
+       
+        this.dicepage = new DicePage(this.numDice);
+       
+      
+        this.buttons = document.getElementsByTagName("button");
+        for(i=0; i<this.buttons.length; i++){
+          this.buttons[i].style.display= "none"
+        };
+       
 
-    this.header = document.getElementsByTagName("h2");
-    for(i=0; i<this.header.length; i++){
-    this.header[i].style.display = "none"
+      });
+      
     }
+  }
 
-    this.numDice = parseInt(event.target.textContent);
-    //skapar en dicePage där tärningarna kommer att vara
-
-  
-   
-    this.dicepage = new DicePage(this.numDice);
-   
-  
-    this.buttons = document.getElementsByTagName("button");
-    for(i=0; i<this.buttons.length; i++){
-      this.buttons[i].style.display= "none"
-    };
-   
-
-  });
-  
-}
-}
+  // this.buttons = document.getElementsByTagName("button");
+  //       for(i=0; i<buttons.length; i++){
+  //         buttons[i].style.display="none";
+         
--- Javascript/Permission.js
+++ Javascript/Permission.js
@@ -20,3 +20,4 @@ Permission.givePermission = function(){
     }
   
 }  
+
